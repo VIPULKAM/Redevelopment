e1def7ac88da8d4098f06da2162bbeb6d95a54e6
VIPULKAM <34189919+VIPULKAM@users.noreply.github.com>
2025-03-28 22:12:08 -0500

    sync app.py

--- redevelopment-calculator/app.py
+++ redevelopment-calculator/app.py
@@ -1,6 +1,7 @@
 # File: main.py
 import streamlit as st
 import matplotlib.pyplot as plt
+#from visitor_counter import display_visitor_counter
 from visitor_analytics import display_visitor_counter
 
 # Import modules
